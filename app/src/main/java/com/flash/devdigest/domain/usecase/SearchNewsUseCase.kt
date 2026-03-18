de9e7b7dffbdebcc11c88af459460c6b78f27774
Sridhar Prasath <96571944+sridharprasath94@users.noreply.github.com>
2026-03-18 09:22:23 +0100

    Make search news return Flow<PagingData<News>>

--- app/src/main/java/com/flash/devdigest/domain/usecase/SearchNewsUseCase.kt
+++ app/src/main/java/com/flash/devdigest/domain/usecase/SearchNewsUseCase.kt
@@ -1,15 +1,16 @@
 package com.flash.devdigest.domain.usecase
 
 
-import com.flash.devdigest.core.Result
+import androidx.paging.PagingData
 import com.flash.devdigest.domain.model.News
 import com.flash.devdigest.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SearchNewsUseCase @Inject constructor(
     private val repository: NewsRepository
 ) {
-    suspend operator fun invoke(query: String): Result<List<News>> {
-        return repository.searchNews(query)
+    operator fun invoke(query: String): Flow<PagingData<News>> {
+        return repository.observeSearchNews(query)
     }
 }
